72a7d24ed178e99b65abdb236b7939d7b4e55117
slikc <82408235+slikc@users.noreply.github.com>
2022-11-26 21:51:01 +0000

    new shit + shanilev

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import string, random, requests, time
-from colorama import Fore, Back, Style
+import string, random, requests, time, json, ctypes
+from colorama import init, Fore, Back, Style
+requests.packages.urllib3.disable_warnings()
+counter = 0
 
 
-for i in range(10000):
+def main():
+    global counter
     email = ("").join(random.choices(string.ascii_letters + string.digits, k = 8)) + "@gmail.com"
     password = ("").join(random.choices(string.ascii_letters + string.digits, k = 8))
-    data = f"birth_day=1&birth_month=01&birth_year=1970&collect_personal_info=undefined&creation_flow=&creation_point=https://www.spotify.com/uk/&displayname=unknown&email={email}&gender=neutral&iagree=1&key=a1e486e2729f46d6bb368d6b2bcda326&password={password}&password_repeat={password}&platform=www&referrer=&send-email=1&thirdpartyemail=0&fb=0"
+    data = f"birth_day=1&birth_month=01&birth_year=1970&collect_personal_info=undefined&creation_flow=&creation_point=https://www.spotify.com/uk/&displayname=gay levy&email={email}&gender=neutral&iagree=1&key=a1e486e2729f46d6bb368d6b2bcda326&password={password}&password_repeat={password}&platform=www&referrer=&send-email=1&thirdpartyemail=0&fb=0"
     session = requests.Session()
     headers = {
                 "Accept": "*/*",
@@ -14,7 +17,7 @@ for i in range(10000):
                 "Referer": "https://www.spotify.com/",
                 "accept-language": "en-US,en;q=0.9",
     }
-    create = session.post("https://spclient.wg.spotify.com/signup/public/v1/account/", data=data, headers=headers)
+    create = session.post("https://spclient.wg.spotify.com/signup/public/v1/account/", data=data, headers=headers, verify=False)
     if create.status_code == 200:
         if "login_token" in create.text:
             print(f"{Fore.GREEN}[+]{Fore.RESET} GOT LOGIN TOKEN")
@@ -22,7 +25,7 @@ for i in range(10000):
             print(f"{Fore.GREEN}[+]{Fore.RESET} CREATED ACCOUNT: " + email + ":" + password)
 
 
-    r = session.get("https://www.spotify.com/uk/signup/?forward_url=https://accounts.spotify.com/en/status&sp_t_counter=1")
+    r = session.get("https://www.spotify.com/uk/signup/?forward_url=https://accounts.spotify.com/en/status&sp_t_counter=1", verify=False)
     #get the csrf token from the text "csrfToken":"<token>" by splitting the text
     csrf = r.text.split('"csrfToken":"')[1].split('"')[0]
     if csrf == "":
@@ -36,7 +39,7 @@ for i in range(10000):
                 "X-CSRF-Token": csrf,
                 "Host": "www.spotify.com"
                 }
-        session.post("https://www.spotify.com/api/signup/authenticate", headers=headers, data="splot=" + login_token)
+        session.post("https://www.spotify.com/api/signup/authenticate", headers=headers, data="splot=" + login_token, verify=False)
         headers = {
                 "accept": "application/json",
                 "Accept-Encoding": "gzip, deflate, br",
@@ -48,7 +51,7 @@ for i in range(10000):
                 "Referer": "https://open.spotify.com/"
             }
         try:
-            r = session.get("https://open.spotify.com/get_access_token?reason=transport&productType=web_player", headers = headers)
+            r = session.get("https://open.spotify.com/get_access_token?reason=transport&productType=web_player", headers = headers, verify=False)
             return r.json()["accessToken"]
         except:
             return None
@@ -68,12 +71,17 @@ for i in range(10000):
                 "spotify-app-version": "1.1.52.204.ge43bc405",
                 "authorization": "Bearer {}".format(auth_token),
             }
-    followuser = session.put("https://api.spotify.com/v1/me/following?type=user&ids="+ idd, headers=headers)
-    accountstats = requests.get(f"https://spclient.wg.spotify.com/user-profile-view/v3/profile/{idd}?playlist_limit=0&artist_limit=0&episode_limit=0&market=from_token", headers=headers).json()
+    followuser = session.put("https://api.spotify.com/v1/me/following?type=user&ids="+ idd, headers=headers, verify=False)
+    accountstats = requests.get(f"https://spclient.wg.spotify.com/user-profile-view/v3/profile/{idd}?playlist_limit=0&artist_limit=0&episode_limit=0&market=from_token", headers=headers, verify=False).json()
     accountstats = accountstats["followers_count"]
     print(f"{Fore.BLUE}[INFO]{Fore.RESET} {Fore.CYAN}Current User Follows: {accountstats}")
     if followuser.status_code == 204:
         print(f"{Fore.GREEN}[+]{Fore.RESET} FOLLOWED USER")
+        ctypes.windll.kernel32.SetConsoleTitleW(f"Spotify Account Creator | Estimated Created: {accountstats + counter} | By #slikc0001")
+        counter += 1
 
     else:
         print(f"{Fore.RED}[-]{Fore.RESET} FAILED TO FOLLOW USER")
+
+if __name__ == "__main__":
+    main()
---
+++ run.py
@@ -0,0 +1,19 @@
+import threading
+from main import main
+
+counter = 0
+
+threads = int(input("How many threads do you want to run? "))
+
+def thread_starter():
+    global counter
+    main()
+    counter += 1
+    
+while True:
+    if threading.active_count() <= threads:
+        try:
+            threading.Thread(target = thread_starter).start()
+            print("Thread started. Total threads: " + str(threading.active_count()))
+        except:
+            pass
